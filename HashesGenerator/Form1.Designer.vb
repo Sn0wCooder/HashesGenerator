14b836c74249a155e9f96cc9f00dfbb2a731fb5d
unknown <leoalfreducci@gmail.com>
2015-09-27 22:26:15 +0200

    Initial commit

---
+++ HashesGenerator/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SHA256TextBox = New System.Windows.Forms.TextBox()
+        Me.SHA1TextBox = New System.Windows.Forms.TextBox()
+        Me.MD5TextBox = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FileTextBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.SaveButton = New System.Windows.Forms.Button()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SHA384TextBox = New System.Windows.Forms.TextBox()
+        Me.SHA512TextBox = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "File size:"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.AutoUpgradeEnabled = False
+        Me.OpenFileDialog1.Filter = "*.*|*.*"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.SHA512TextBox)
+        Me.GroupBox1.Controls.Add(Me.SHA384TextBox)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.SHA256TextBox)
+        Me.GroupBox1.Controls.Add(Me.SHA1TextBox)
+        Me.GroupBox1.Controls.Add(Me.MD5TextBox)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 39)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(462, 176)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "File info"
+        '
+        'SHA256TextBox
+        '
+        Me.SHA256TextBox.Location = New System.Drawing.Point(68, 97)
+        Me.SHA256TextBox.Name = "SHA256TextBox"
+        Me.SHA256TextBox.ReadOnly = True
+        Me.SHA256TextBox.Size = New System.Drawing.Size(384, 20)
+        Me.SHA256TextBox.TabIndex = 6
+        '
+        'SHA1TextBox
+        '
+        Me.SHA1TextBox.Location = New System.Drawing.Point(68, 71)
+        Me.SHA1TextBox.Name = "SHA1TextBox"
+        Me.SHA1TextBox.ReadOnly = True
+        Me.SHA1TextBox.Size = New System.Drawing.Size(384, 20)
+        Me.SHA1TextBox.TabIndex = 5
+        '
+        'MD5TextBox
+        '
+        Me.MD5TextBox.Location = New System.Drawing.Point(68, 45)
+        Me.MD5TextBox.Name = "MD5TextBox"
+        Me.MD5TextBox.ReadOnly = True
+        Me.MD5TextBox.Size = New System.Drawing.Size(384, 20)
+        Me.MD5TextBox.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 100)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "SHA-256:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "SHA-1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 48)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "MD5:"
+        '
+        'FileTextBox
+        '
+        Me.FileTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.FileTextBox.Name = "FileTextBox"
+        Me.FileTextBox.Size = New System.Drawing.Size(519, 20)
+        Me.FileTextBox.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(537, 10)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(31, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Enabled = False
+        Me.ClearButton.Location = New System.Drawing.Point(480, 109)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(88, 23)
+        Me.ClearButton.TabIndex = 4
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'SaveButton
+        '
+        Me.SaveButton.Enabled = False
+        Me.SaveButton.Location = New System.Drawing.Point(480, 68)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(88, 35)
+        Me.SaveButton.TabIndex = 5
+        Me.SaveButton.Text = "Save into text document"
+        Me.SaveButton.UseVisualStyleBackColor = True
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(480, 39)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(88, 23)
+        Me.CalculateButton.TabIndex = 6
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(480, 138)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(88, 23)
+        Me.ExitButton.TabIndex = 7
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.AutoUpgradeEnabled = False
+        Me.SaveFileDialog1.FileName = "hashes"
+        Me.SaveFileDialog1.Filter = "Text document (TXT File)|*.txt|Rich Text File (RTF)|*.rtf|All files|*.*"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(9, 126)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(53, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "SHA-384:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(9, 152)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(53, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "SHA-512:"
+        '
+        'SHA384TextBox
+        '
+        Me.SHA384TextBox.Location = New System.Drawing.Point(68, 123)
+        Me.SHA384TextBox.Name = "SHA384TextBox"
+        Me.SHA384TextBox.ReadOnly = True
+        Me.SHA384TextBox.Size = New System.Drawing.Size(384, 20)
+        Me.SHA384TextBox.TabIndex = 9
+        '
+        'SHA512TextBox
+        '
+        Me.SHA512TextBox.Location = New System.Drawing.Point(68, 149)
+        Me.SHA512TextBox.Name = "SHA512TextBox"
+        Me.SHA512TextBox.ReadOnly = True
+        Me.SHA512TextBox.Size = New System.Drawing.Size(384, 20)
+        Me.SHA512TextBox.TabIndex = 10
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(514, 202)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(54, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Follow me"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(573, 221)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.SaveButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.FileTextBox)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "HashesGenerator"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents SHA256TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents SHA1TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents MD5TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents FileTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents ClearButton As System.Windows.Forms.Button
+    Friend WithEvents SaveButton As System.Windows.Forms.Button
+    Friend WithEvents CalculateButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents SaveFileDialog1 As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents SHA512TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents SHA384TextBox As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+
+End Class
---
+++ HashesGenerator/Form1.vb
@@ -0,0 +1,129 @@
+﻿Imports System
+Imports System.Security.Cryptography
+Imports System.Text
+Imports System.IO
+
+Public Class Form1
+#Region "Raccourcis pour la fonction principale hash_generator"
+    Function md5_hash(ByVal file_name As String)
+        Return hash_generator("md5", file_name)
+    End Function
+
+    Function sha_1(ByVal file_name As String)
+        Return hash_generator("sha1", file_name)
+    End Function
+
+    Function sha_256(ByVal file_name As String)
+        Return hash_generator("sha256", file_name)
+    End Function
+
+    Function sha_384(ByVal file_name As String)
+        Return hash_generator("sha384", file_name)
+    End Function
+
+    Function sha_512(ByVal file_name As String)
+        Return hash_generator("sha512", file_name)
+    End Function
+#End Region
+
+    Function hash_generator(ByVal hash_type As String, ByVal file_name As String)
+        Dim hash
+        If hash_type.ToLower = "md5" Then
+            hash = MD5.Create
+        ElseIf hash_type.ToLower = "sha1" Then
+            hash = SHA1.Create()
+        ElseIf hash_type.ToLower = "sha256" Then
+            hash = SHA256.Create()
+        ElseIf hash_type.ToLower = "sha384" Then
+            hash = SHA384.Create
+        ElseIf hash_type.ToLower = "sha512" Then
+            hash = SHA512.Create
+        Else
+            MsgBox("Type de hash inconnu : " & hash_type, MsgBoxStyle.Critical)
+            Return False
+        End If
+        Dim hashValue() As Byte
+        Dim fileStream As FileStream = File.OpenRead(file_name)
+        fileStream.Position = 0
+        hashValue = hash.ComputeHash(fileStream)
+        Dim hash_hex = PrintByteArray(hashValue)
+        fileStream.Close()
+        Return hash_hex
+    End Function
+
+    Public Function PrintByteArray(ByVal array() As Byte)
+        Dim hex_value As String = ""
+        Dim i As Integer
+        For i = 0 To array.Length - 1
+            hex_value += array(i).ToString("X2")
+        Next i
+        Return hex_value.ToLower
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            FileTextBox.Text = OpenFileDialog1.FileName
+            MD5TextBox.Text = md5_hash(OpenFileDialog1.FileName)
+            SHA1TextBox.Text = sha_1(OpenFileDialog1.FileName)
+            SHA256TextBox.Text = sha_256(OpenFileDialog1.FileName)
+            SHA384TextBox.Text = sha_384(OpenFileDialog1.FileName)
+            SHA512TextBox.Text = sha_512(OpenFileDialog1.FileName)
+            Label1.Text = "File size: " & My.Computer.FileSystem.GetFileInfo(OpenFileDialog1.FileName).Length & " bytes"
+            ClearButton.Enabled = True
+            SaveButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        MD5TextBox.Clear()
+        SHA1TextBox.Clear()
+        SHA256TextBox.Clear()
+        FileTextBox.Clear()
+        SHA512TextBox.Clear()
+        SHA384TextBox.Clear()
+        Label1.Text = "File size:"
+        SaveButton.Enabled = False
+        ClearButton.Enabled = False
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        End
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles SaveButton.Click
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            If System.IO.File.Exists(SaveFileDialog1.FileName) Then
+                System.IO.File.Delete(SaveFileDialog1.FileName)
+            End If
+            Dim FileName As String = System.IO.Path.GetFileName(FileTextBox.Text)
+            Dim Text As String = "File name: " & FileName & vbCrLf & vbCrLf & "MD5: " & MD5TextBox.Text & vbCrLf & "SHA-1: " & SHA1TextBox.Text & vbCrLf & "SHA-256: " & SHA256TextBox.Text & vbCrLf & _
+               "SHA-384: " & SHA384TextBox.Text & vbCrLf & "SHA-512: " & SHA512TextBox.Text & vbCrLf & Label1.Text
+            My.Computer.FileSystem.WriteAllText(SaveFileDialog1.FileName, Text, True)
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        If System.IO.File.Exists(FileTextBox.Text) Then
+            MD5TextBox.Text = md5_hash(FileTextBox.Text)
+            SHA1TextBox.Text = sha_1(FileTextBox.Text)
+            SHA256TextBox.Text = sha_256(FileTextBox.Text)
+            SHA384TextBox.Text = sha_384(FileTextBox.Text)
+            SHA512TextBox.Text = sha_512(FileTextBox.Text)
+            Label1.Text = "File size: " & My.Computer.FileSystem.GetFileInfo(FileTextBox.Text).Length & " bytes"
+            SaveButton.Enabled = True
+            ClearButton.Enabled = True
+        Else
+            MsgBox("File not found!", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
+
+    Private Sub FileTextBox_TextChanged(sender As Object, e As EventArgs) Handles FileTextBox.TextChanged
+        If Not FileTextBox.Text = Nothing Then
+            ClearButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("http://leoalfred.altervista.org/twitter.html")
+    End Sub
+End Class
